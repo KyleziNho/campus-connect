e89227aa45ff4ea6e89b4bf4d6e8075f37101d5c
Your Name <your.email@example.com>
2024-10-30 13:41:05 +0000

    Fix next.config.js Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,15 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
-    domains: [
-      'media-photos.depop.com',
-      'images1.vinted.net',
-      'media.istockphoto.com',
-      'placehold.co',
-      'firebasestorage.googleapis.com'
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com'
+      },
+      {
+        protocol: 'https', 
+        hostname: 'media-photos.depop.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images1.vinted.net'
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.istockphoto.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co'
+      }
     ]
   },
   env: {
@@ -18,15 +31,8 @@ const nextConfig = {
     NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-    };
-    return config;
-  },
-}
-
-module.exports = nextConfig
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  }
+ }
+ 
+ module.exports = nextConfig
